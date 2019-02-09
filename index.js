032226a58dfb28da596f39aafa6da80b6988e398
NatashaDianeey <ndianeey@gmail.com>
2019-02-08 20:52:45 -0600

    Calculadora con operadores basicos en Javascript

---
+++ index.js
@@ -0,0 +1,8 @@
+const math = require('./math');
+const operaciones = require('./math');
+
+
+console.log(math.suma(10, 2));
+console.log(math.resta(8, 2));
+console.log(math.multiplicacion(6, 2));
+console.log(math.division(4, 2));
---
+++ math.js
@@ -0,0 +1,31 @@
+// let nombre = "Natasha Dianeeey";
+
+// saludar(nombre);
+
+// function nombre(parametroFuncion) {
+//     console.log('Hola ' + parametroFuncion); 
+// }
+
+
+
+module.exports = {
+    // Funcion anonima, por que no tiene nombre
+    suma: function (a, b) {
+        return a + b;
+    },
+    resta: function (a, b) {
+        return a - b;
+    },
+    multiplicacion: function (a, b) {
+        return a * b;
+    },
+    division: function (a, b) {
+        return a / b;
+    }
+}
+
+// object.propiedad
+// // Cuando tenemos un valor es propiedad
+// // Cuando tenemos una propiedad es un metodo
+
+// module.exports 
